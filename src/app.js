3c7a503b1e1a2aae2bcdfaffb8e9e862865bbd3f
gxc <1003823149@qq.com>
2022-09-24 00:58:11 +0800

    change webpack

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
-import createRouter from './router/index';
-import createStore from './vuex/store'
+// import createRouter from './router/index';
+// import createStore from './vuex/store'
 export default () => { // 为了保证实例的唯一性所以导出一个创建实例的函数
-    let router = createRouter();
-    let store = createStore();
+    // let router = createRouter();
+    // let store = createStore();
     const app = new Vue({
-        router,
-        store,
+        // router,
+        // store,
         render: h => h(App)
     });
-    return { app, router, store};
+    return { app};
 };
